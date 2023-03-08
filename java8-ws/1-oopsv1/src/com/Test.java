3eb873cad3d0e2cb487117880a08da1f2ca4f2cf
Praveen Reddy <praveen@Praveens-MacBook-Pro.local>
2023-03-08 10:36:59 +0530

    class

---
+++ java8-ws/1-oopsv1/src/com/Test.java
@@ -0,0 +1,89 @@
+package com;
+
+
+public class Test {
+	
+	
+	/**
+	 *  class
+	 *  =======================
+	 *  -> a class is a blue print of an object.
+	 *     ex: Fruit, Employee, Vehicle, Animal,Human,Country
+	 * 
+	 *    what max a class can have?
+	 *    ==================================
+	 *    1.variables
+	 *    2.methods
+	 *    3.constructors
+	 *    4.blocks
+	 *    5.inner class
+	 *    5.interfaces
+	 *    6.enums
+	 * 
+	 *    1.variables / fields
+	 *    =======================
+	 *    1.instance
+	 *    2.static
+	 *    3.final
+	 *    4.volatile
+	 *    5.transient
+	 *    
+	 *    2.methods
+	 *    ========================
+	 *    1.instance
+	 *    2.static
+	 *    3.final
+	 *    4.native
+	 *    5.abstract
+	 *    
+	 *    3.constructors
+	 *    =================
+	 *    1.no argument 
+	 *    2.argument based
+	 *    
+	 *    4.blocks
+	 *    =================
+	 *    1.instance
+	 *    2.static
+	 *    
+	 *    5.inner class
+	 *    =================
+	 *    1.inner class
+	 *    2.Anonymous inner class
+	 *    
+	 *    5.interfaces
+	 *    ================
+	 *    1.interfaces
+	 *    2.multiple interfaces
+	 *    
+	 *    
+	 *    
+	 *    instance data
+	 *    ===================
+	 *    -> instance data can be unique to a object
+	 *   
+	 *    static data
+	 *    ===================
+	 *    -> static data is sharable b/w objects
+	 *    
+	 *    
+	 *    methods
+	 *    ==========
+	 *    1.instance methods
+	 *    2.static methods
+	 *    
+	 *    
+	 * 
+	 * 
+	 */
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello java!");
+
+	}
+
+}
